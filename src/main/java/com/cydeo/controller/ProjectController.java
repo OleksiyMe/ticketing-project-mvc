38b6cfb961c407c427e448a3865e35c91d36b983
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-10-02 16:40:43 -0400

    ProjectDTO save & delete done

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,28 +1,55 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import com.cydeo.service.impl.ProjectServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
 
+    private final UserService userService;
+    private final ProjectService projectService;
+
+    public ProjectController(UserService userService, ProjectService projectService) {
+        this.userService = userService;
+        this.projectService = projectService;
+    }
 
     @GetMapping("/create")
-     public String createProject(Model model){
+    public String createProject(Model model) {
 
 
         model.addAttribute("project", new ProjectDTO());
 
-        model.addAttribute("managers", )
+        model.addAttribute("managers", userService.findAll());
+
+        model.addAttribute("projects", projectService.findAll());
 
 
         return "/project/create";
     }
 
+    @PostMapping("/create")
+    public String createProject1(@ModelAttribute("project") ProjectDTO project, Model model) {
+
+        projectService.save(project);
+
+        return "redirect:/project/create";
+    }
+
+
+    @GetMapping("/delete/{projeCtcode}")
+    public String deleteProject(@PathVariable String projeCtcode) {
+        projectService.deleteById(projeCtcode);
+
+        return "redirect:/project/create";
+    }
 
 
 }
--- src/main/java/com/cydeo/enums/Status.java
+++ src/main/java/com/cydeo/enums/Status.java
@@ -2,14 +2,15 @@ package com.cydeo.enums;
 
 public enum Status {
 
-    OPEN("Open"), IN_PROGRESS("In Progress"), COMPLETED("Completed");
-
+    OPEN("Open"),IN_PROGRESS("In Progress"),COMPLETE("Completed");
 
     private final String value;
 
-    Status(String value) {
-     this.value=value;
+    public String getValue() {
+        return value;
     }
 
-
+    Status(String value) {
+        this.value = value;
+    }
 }
